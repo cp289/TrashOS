f4484498113461b1f1d9ff26a85298dc9ad27bc9
Charles Parham <cparham289@gmail.com>
2019-10-02 13:19:01 -0400

    Fix paging

--- kernel/arch/i386/kernel.c
+++ kernel/arch/i386/kernel.c
@@ -25,22 +25,17 @@ void kernel_main(void)
      */
     gdt_init();
     idt_init();
+    page_init();
     vga_init();
 
-    volatile int a = 0;
-    int test = 3/a;
-    kprintf("Div: %d\n", test);
-
-    //page_init();
-
-    //kprintf("CR0: %p\n", get_cr0());
-    //kprintf("CR2: %p\n", get_cr2());
-    //kprintf("CR3: %p\n", get_cr3());
-    //kprintf("CR4: %p\n", get_cr4());
-    //kprintf("CS: %p\n", dump_cs());
-    //kprintf("DS: %p\n", dump_ds());
-    //kprintf("ES: %p\n", dump_es());
-    //kprintf("FS: %p\n", dump_fs());
-    //kprintf("GS: %p\n", dump_gs());
-    //kprintf("SS: %p\n", dump_ss());
+    kprintf("CR0: %p\n", get_cr0());
+    kprintf("CR2: %p\n", get_cr2());
+    kprintf("CR3: %p\n", get_cr3());
+    kprintf("CR4: %p\n", get_cr4());
+    kprintf("CS:  %p\n", dump_cs());
+    kprintf("DS:  %p\n", dump_ds());
+    kprintf("ES:  %p\n", dump_es());
+    kprintf("FS:  %p\n", dump_fs());
+    kprintf("GS:  %p\n", dump_gs());
+    kprintf("SS:  %p\n", dump_ss());
 }
--- kernel/arch/i386/page.c
+++ kernel/arch/i386/page.c
@@ -5,13 +5,6 @@
 #include "page.h"
 #include "std.h"
 
-//static page_t new_page(void *address, uint32_t flags)
-//{
-//    page_t entry = (uintptr_t)address;
-//    entry |= flags & 0xfff;
-//    return entry;
-//}
-
 static page_dir_t _Alignas(PAGE_SIZE) kernel_page_dir;
 static page_table_t _Alignas(PAGE_SIZE) kernel_page_table;
 
@@ -22,10 +15,11 @@ void page_init(void)
         kernel_page_dir[i] = PAGE_WRITE;
     }
 
+    // Initialize first page table
     kernel_page_dir[0] |= (uintptr_t)&kernel_page_table | PAGE_PRESENT;
 
     for (int i = 0; i < 1024; i++) {
-        kernel_page_table[0] = (i << 12) | PAGE_WRITE | PAGE_PRESENT;
+        kernel_page_table[i] = (i << 12) | PAGE_WRITE | PAGE_PRESENT;
     }
 
     page_load_dir(&kernel_page_dir);
