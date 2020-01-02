3ab7de24a93c84b0da2a5ec045999aed5903f3a2
Charles Parham <cparham289@gmail.com>
2020-01-02 00:19:37 -0500

    Refactor init code, place read-only protections on .text and .rodata sections

--- kernel/arch/i386/init.c
+++ kernel/arch/i386/init.c
@@ -3,13 +3,9 @@
  * a higher-half kernel virtual address space
  */
 
-// TODO
-#include "asm.h"
-
 #include "io.h"
 #include "page.h"
 #include "mem.h"
-#include "std.h"
 #include "vga.h"
 
 extern void kernel_main(void);      // Kernel entry point
@@ -23,9 +19,9 @@ page_entry_t _Alignas(PAGE_SIZE) init_page_dir[PAGE_ENTRIES];
 static uintptr_t *init_page_table_lookup;
 
 static uintptr_t init_kernel_brk;
-static uintptr_t init_brk;              // See description for init_page_map()
-static uintptr_t init_vga_buffer_vma;   // Mapped address for vga buffer
-static uintptr_t init_page_table_offset;    // Offset for new page table
+static uintptr_t init_brk;               // See description for init_page_map()
+static uintptr_t init_vga_buffer_vma;    // Mapped address for vga buffer
+static uintptr_t init_page_table_offset; // Offset for new page table
 
 // Erase page (overwrite with zeros)
 static void init_page_clear(uintptr_t pma)
@@ -52,7 +48,7 @@ static uintptr_t init_page_new(void)
  * allocations.
  * NOTE: This routine simply maps the address without altering flags
  */
-static void init_page_map(uintptr_t vma, uintptr_t pma)
+static void init_page_map(uintptr_t vma, uintptr_t pma, uintptr_t flags)
 {
     const uintptr_t page_dir_idx = vma >> 22;
     const uintptr_t page_table_idx = vma >> 12 & 0x3ff;
@@ -70,21 +66,7 @@ static void init_page_map(uintptr_t vma, uintptr_t pma)
     } else {
         table[page_table_idx] = (table[page_table_idx] & 0xfff) | pma;
     }
-}
-
-/**
- * For simplicity of implementation, we assume that the page table to be
- * configured exists and is mapped properly in the page directory. This is fine
- * for init implementation, but the corresponding method in the initialized
- * kernel should be more robust.
- */
-static void init_page_set_flags(uintptr_t vma, uintptr_t flags)
-{
-    const uintptr_t page_dir_idx = vma >> 22;
-    const uintptr_t page_table_idx = vma >> 12 & 0x3ff;
-
-    page_entry_t *table = (void*)(init_page_dir[page_dir_idx] >> 12 << 12);
-    table[page_table_idx] = (table[page_table_idx] >> 12 << 12) | flags;
+    table[page_table_idx] |= flags;
 }
 
 // Map kernel into virtual address space
@@ -92,8 +74,8 @@ static void init_map_kernel(void)
 {
     init_brk = KERNEL_END - KERNEL_VMA + INIT_BSS_END;
 
-    // We start placing new page tables after the kernel BSS section followed by
-    // the page directory
+    // We start placing new page tables after the kernel BSS section and the
+    // page directory
     init_page_table_offset = BSS_END + PAGE_SIZE;
 
     // Initialize all page directory entries to writable
@@ -107,24 +89,25 @@ static void init_map_kernel(void)
     // Identity map init section
     uintptr_t i;
     for (i = INIT_START; i < INIT_BSS_END; i += PAGE_SIZE) {
-        init_page_map(i, i);
-        init_page_set_flags(i, PAGE_WRITE | PAGE_PRESENT);
+        init_page_map(i, i, PAGE_WRITE | PAGE_PRESENT);
     }
 
-    // Map kernel components known in the linker script
-    for (i = 0; i < KERNEL_END - TEXT_START; i += PAGE_SIZE) {
-        const uintptr_t pma = INIT_BSS_END + i;
-        const uintptr_t vma = KERNEL_VMA + i;
-        init_page_map(vma, pma);
-        init_page_set_flags(vma, PAGE_WRITE | PAGE_PRESENT);
+    // Map .text and .rodata (read-only)
+    for (i = 0; i < RODATA_END - TEXT_START; i += PAGE_SIZE) {
+        init_page_map(TEXT_START + i, INIT_BSS_END + i, PAGE_PRESENT);
+    }
+
+    // Map .data and .bss (read/write)
+    for ( ; i < BSS_END - TEXT_START; i += PAGE_SIZE) {
+        init_page_map(TEXT_START + i, INIT_BSS_END + i,
+                      PAGE_WRITE | PAGE_PRESENT);
     }
 
     init_kernel_brk = BSS_END;
 
     // Map page directory
-    // TODO copy this to preserve memory continuity?
-    init_page_map(init_kernel_brk, (uintptr_t)&init_page_dir);
-    init_page_set_flags(init_kernel_brk, PAGE_WRITE | PAGE_PRESENT);
+    init_page_map(init_kernel_brk, (uintptr_t)&init_page_dir,
+                  PAGE_WRITE | PAGE_PRESENT);
 
     init_kernel_brk += PAGE_SIZE;
 
@@ -140,7 +123,8 @@ static void init_map_kernel(void)
 
     // Determine if more page tables are needed
     size_t n_new_tables;
-    uintptr_t kernel_end_dir_aligned = align(kernel_end_pma, PAGE_ENTRIES * PAGE_SIZE);
+    uintptr_t kernel_end_dir_aligned = align(kernel_end_pma,
+                                             PAGE_ENTRIES * PAGE_SIZE);
     if (init_brk < kernel_end_dir_aligned) {
         n_new_tables = 0;
     } else {
@@ -149,26 +133,27 @@ static void init_map_kernel(void)
     }
 
     // Map all dynamically-allocated page tables into kernel space
-    for (i = 0; i < init_alloc_size + n_new_tables * PAGE_SIZE; i += PAGE_SIZE) {
-        init_page_map(init_kernel_brk + i, kernel_end_pma + i);
-        init_page_set_flags(init_kernel_brk + i, PAGE_WRITE | PAGE_PRESENT);
+    for (i = 0; i < init_alloc_size + n_new_tables * PAGE_SIZE;
+         i += PAGE_SIZE) {
+        init_page_map(init_kernel_brk + i, kernel_end_pma + i,
+                      PAGE_WRITE | PAGE_PRESENT);
     }
 
     init_kernel_brk += i;
 
     // Map VGA space
     init_vga_buffer_vma = init_kernel_brk;
-    for (i = 0; i < VGA_WIDTH_DEFAULT * VGA_HEIGHT_DEFAULT * sizeof(vga_entry_t);
+    for (i = 0;
+         i < VGA_WIDTH_DEFAULT * VGA_HEIGHT_DEFAULT * sizeof(vga_entry_t);
          i += PAGE_SIZE) {
-        init_page_map(init_kernel_brk + i, (uintptr_t)VGA_PADDR_BUFFER + i);
-        init_page_set_flags(init_kernel_brk + i, PAGE_WRITE | PAGE_PRESENT);
+        init_page_map(init_kernel_brk + i, (uintptr_t)VGA_PADDR_BUFFER + i,
+                      PAGE_WRITE | PAGE_PRESENT);
     }
 
     init_kernel_brk += i;
 
     // Lastly, we map a page for the kernel stack
-    init_page_map(-(intptr_t)PAGE_SIZE, init_brk);
-    init_page_set_flags(-(intptr_t)PAGE_SIZE, PAGE_WRITE | PAGE_PRESENT);
+    init_page_map(-(intptr_t)PAGE_SIZE, init_brk, PAGE_WRITE | PAGE_PRESENT);
 }
 
 void init(void)
